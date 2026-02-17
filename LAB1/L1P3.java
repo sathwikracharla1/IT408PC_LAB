1f2db1e98d7b0fa8dece1d05260f77e96110f2bf
Sathwik Racharla <sathwikracharla1@gmail.com>
2026-02-17 23:30:54 +0530

    Add files via upload

---
+++ LAB1/L1P3.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+
+class L1P3 {
+    public static void main(String args[]) {
+        int x = 10;
+        double D = 15.18;
+        float y = (float) D;
+        double d = 100.50;
+        char ch = 'a';
+        boolean b = true;
+        byte by = 6;
+        short s = 12;
+        long l = 55;
+
+        System.out.println(
+            "Integer value: " + x +
+            "\nFloat value: " + y +
+            "\nDouble value: " + d +
+            "\nCharacter value: " + ch +
+            "\nBoolean value: " + b +
+            "\nByte value: " + by +
+            "\nShort value: " + s +
+            "\nLong value: " + l
+        );
+    }
+}
