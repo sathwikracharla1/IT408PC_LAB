8bc635f177da24c691cfc6f985ab113cf8edc08c
Sathwik Racharla <sathwikracharla1@gmail.com>
2026-02-17 23:33:53 +0530

    L3P1.java

---
+++ LAB3/L3P1.java
@@ -0,0 +1,16 @@
+class L3P1{
+	static int view;
+	public static void display(){
+	System.out.println(view);
+	}
+	public static int show(){
+	return (view);	 
+	}
+	public static void main(String args[]){
+	view=10;	
+	display();
+	System.out.println(show());
+	}
+}
+	
+
